0c19de0204914a86fa5de4f308e6b526af64936c
Upasana <upasana.dhameliya@gmail.com>
2021-06-08 10:04:18 +0530

    Reverted the settings to production

--- srifintechbackend/settings.py
+++ srifintechbackend/settings.py
@@ -28,12 +28,12 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
                        
 #                            > make this value True in for local run
-DEBUG = os.getenv("DEBUG", "True") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 #                            > make this value True in for local run
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "True") == "True"
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 # Application definition
 
